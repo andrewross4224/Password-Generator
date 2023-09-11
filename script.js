25db1878e3b39432913f29f5cbd92eec537dead4
Andrew Ross <andrew.ross4224@gmail.com>
2023-09-10 20:52:09 -0400

    changed code to optimize randomization

--- script.js
+++ script.js
@@ -6,17 +6,15 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-// printing password in box
-  passwordText.value = yourpassword;
+  // printing password in box
+  passwordText.value = finalpassword;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// setting base for yourpassword
-var yourpassword = "";
 // setting length to base
-var length ="";
+var length = "";
 // setting values to base
 var uppercase = false;
 var lowercase = false;
@@ -34,55 +32,51 @@ var numberarray = number.split("");
 var specialarray = specialoptions.split("");
 // putting arrays in array
 var selectedarray = [upperarray, lowerarray, numberarray, specialarray];
-// stores true values from criteria
-var optionsarray = [];
+// setting base for final password
+var finalpassword = ""
 
-function arraystorage(){
+function arraystorage() {
   var criteria = [uppercase, lowercase, numeric, special];
-// checks if value is true for criteria and pushes to options array if it is
-  for(var i = 0; i < criteria.length; i++) {
-    if(criteria[i] === true){
-      optionsarray.push(selectedarray[i]);
+  // resets value of password in case site is used again without refresh
+  finalpassword = ""
+  // checks if value is true for criteria and if it is adds a random character from that array not to exceed the length the user input
+  for (var j = 0; j < length; j++) {
+    for (var i = 0; i < criteria.length; i++) {
+      if (criteria[i] === true && finalpassword.length < length) {
+        var randomchar = Math.floor(Math.random() * selectedarray[i].length);
+        finalpassword = finalpassword + selectedarray[i][randomchar];
+      };
     };
   };
-  passwordrandom();
+  return finalpassword;
 };
 function generatePassword() {
-//reseting array and password if button is clicked again 
-  yourpassword = ''
+  //reseting array and password if button is clicked again 
   optionsarray = [];
   length = prompt("Enter a length between 8-20");
-    if (isNaN(length) === true){
-// user entered a value that was not a number
-      alert("Invalid length. Please try again");
-      generatePassword();
+  if (isNaN(length) === true) {
+    // user entered a value that was not a number
+    alert("Invalid length. Please try again");
+    generatePassword();
 
-    } else if(length >=8 && length <=20){
-// getting user imput on password criteria
-      alert("Press 'OK' for yes and 'CANCEL' for no");
-      uppercase = confirm("Does your password require uppercase letters?");
-      lowercase = confirm("Does your password require lowercase letters?");
-      numeric = confirm("Does your password require numbers?");
-      special = confirm("Does your password require special characters?");
-          if(uppercase === false && lowercase === false && numeric === false && special === false){
-            alert("You did not select any criteria. Try again.");
-            generatePassword();
-            } else {
-// if all criteria is good call password function
-              arraystorage();
-            };
+  } else if (length >= 8 && length <= 20) {
+    // getting user imput on password criteria
+    alert("Press 'OK' for yes and 'CANCEL' for no");
+    uppercase = confirm("Does your password require uppercase letters?");
+    lowercase = confirm("Does your password require lowercase letters?");
+    numeric = confirm("Does your password require numbers?");
+    special = confirm("Does your password require special characters?");
+    if (uppercase === false && lowercase === false && numeric === false && special === false) {
+      alert("You did not select any criteria. Try again.");
+      generatePassword();
     } else {
-// user entered a value outside of 8-20
-      alert("Invalid length. Please try again");
-      generatePassword();
-
+      // if all criteria is good call array storage
+      return arraystorage();
     };
-};
-// concat for adding values to string and selecting them randomly
- function passwordrandom() {
-  for(var i = 0; i < length; i++){
-    var random = optionsarray[Math.floor(Math.random()* optionsarray.length)];
-    var randomchar = random[Math.floor(Math.random()* random.length)];
-    yourpassword += randomchar
+  } else {
+    // user entered a value outside of 8-20
+    alert("Invalid length. Please try again");
+    generatePassword();
+
   };
 };
